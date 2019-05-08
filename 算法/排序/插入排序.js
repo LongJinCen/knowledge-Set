cb1059eb598627dec7f2ffb13028982bbcb0c3d8
longjincen <2418204619@qq.com>
2019-05-08 17:50:40 +0800

    feat:堆

--- 算法/排序/插入排序.js
+++ 算法/排序/插入排序.js
@@ -8,7 +8,7 @@
 
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-        let key = arr[i]        
+        let key = arr[i]
         let j = i - 1
         while(j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j]
