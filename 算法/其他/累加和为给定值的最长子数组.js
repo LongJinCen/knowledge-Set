925fc4e9e0ea6cb0f3109acea54b37c51d8c261c
longjincen <2418204619@qq.com>
2019-09-03 10:51:01 +0800

    feat: 平衡二叉树和求指定的和的数组长度

---
+++ 算法/其他/累加和为给定值的最长子数组.js
@@ -0,0 +1,71 @@
+// 给定一个数组 arr ,给定一个目标值 aim, 求改数组中和为 aim 的最长的数组长度
+// 思路：从 m ~ n  的累加和为 k, 如果要求和为 aim 的数组，那么只需要求 m ~ n 之间和为 k - aim 的范围，如果结尾位置为 x，
+// 那么从 x + 1 ~ n就是何为 aim 的数组
+
+function maxLength(arr, aim) {
+    if (!arr || arr.length === 0) return
+    let newMap = new Map()
+    // 给定一个初始值，以 -1 位置结尾的数组和为 0
+    map.put(0, -1)
+    let len = 0
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+        if (newMap.get(sum - k)) {
+            len = Math.max(i - map.get(sum - k), len)
+        }
+        if (!newMap.get(sum)) {
+            // 每次把当前位置的 sum 放入到newMap当中，并且只放最早出现的，如果有了就不再更新
+            newMap.set(sum, i)
+        }
+    }
+    return len
+}
+
+// 扩展：
+// 1. 给定一个数组，求奇数和偶数个数相等的最长的子数组
+// 思路：奇数变为 1, 偶数变为 -1, 求何为 0 的最长子数组
+// 2. 一个数组中只有 0, 1, 2, 求一个数组中 1 和 2 长度相同的最长子数组长度为多少
+// 思路：1 还是 1，2变 -1, 求何为 0 的最长子数组
+
+// 3. 一个数组互相异或最终为 0为我们给定的数组。给定一个数组，求一种分割方法，使得分割出来的的所有数组中满足前面给定条件的数组最多。
+// 思路：异或满足交换律，且 0 & n = n, n ^ n = 0。使用动态规划。
+function maxLength(arr) {
+    if (!arr || arr.length === 0) return
+    let dp = []
+    let newMap = new Map()
+    // 给定一个初始值，以 -1 位置结尾的数组和为 0
+    map.put(0, -1)
+    let ans = 0 // 结果
+    let xor = 0 // 当前的异或和
+    for (let i = 0; i < arr.length; i++) {
+        xor ^= arr[i]
+        // 如果该异或值出现过
+        if (newMap.get(xor)) {
+            let pre = newMap.get(xor)
+            //0 ~ i, 当前 i 的 xor 和 pre 的 xor 相等，所以 pre ~ i 这一段的异或和为 0。
+            // 如果位置是 -1 ，那么当前的 dp[i] 为 1, 即从头开始的位置的开始到当前的 i 这一段
+            // 如果不是 -1, 那么 从 pre ~ i 这一段可以划分为一段，因为他们的异或和为 0, 所以当前的 dp[i] = dp[pre] + 1
+            dp[i] = pre === -1 ? 1 : (dp[pre] + 1)
+
+        // 如果当前 i 位置的值没有出现过
+        } else {
+            // 如果没有出现过，而 xor 的和又为 0，那么当前的 dp[i] 为 1
+            if (xor === 0) {
+                dp[i] = 1
+            } else {
+            // 如果 xor 和不为 0，那么他的 dp[i] 和前面一个的 dp 值相等
+                dp[i] =  dp[i - 1]
+            }
+        }
+        if (i > 0) {
+            //  去比较如果前面存在过相同的 xor 的情况和和不存在的情况两种，谁的值最大，取大的那个值
+            dp[i] = Math.max(dp[i - 1], dp[i])
+        }
+        // 每一次都要更新当前的异或和，这里不是只存第一次的，而是存最后一次的，后面的会把前面相同的 xor 为key 的覆盖掉
+        map.put(xor, i)
+        // 记录最大的值
+        ans = Math.max(ans, dp[i])
+    }
+    return ans
+}
--- 算法/数据结构/搜索二叉树.js
+++ 算法/数据结构/搜索二叉树.js
@@ -68,3 +68,115 @@ function AbtractBinarySearchTree(Head) {
         
     }
 }
+
+// 平衡二叉搜索树 例题: https://www.lintcode.com/problem/the-skyline-problem/description
+// 思路：将每一栋大楼的信息拆分: 如[1, 3, 3] 可以拆分为 [1,3,上]和[3,3,下]，表示1位置有一个高度为3的上去，3位置有一个高度为3的下来，
+// 然后根据位置排序, 建立一个 平衡二叉搜索树，遍历排序过后的数组， 记录高度，以 高度为 key, 该高度出现的次数为value，如果是上，该value就加一
+// 如果是下，该 value 就减一。在遍历的过程当中，需要记录每一个位置的最大高度
+// java 代码
+
+public class Building_Outline {
+    // 记录每一个位置信息的节点
+    /**
+     * isUp: 是上还是下
+     * posi: 当前位置
+     * h: 高度
+     */
+    public static class Node {
+        public boolean isUp;
+        public int posi;
+        public int h;
+
+        public Node(boolean boRe, int position, int height) {
+            isUp = boRe;
+            posi = position;
+            h = height;
+        }
+    }
+    // 比较器
+    public static class NodeComparator implements Comparator<Node> {
+        @Override
+        public int compare(Node o1, Node o2) {
+            if (o1.posi != o2.posi) {
+                return o1.posi - o2.posi;
+            }
+            // 如果在一个位置，那么这里规定 上的在前面
+            if (o1.isUp != o2.isUp) {
+                return o1.isUp ? -1 : 1;
+            }
+            return 0
+        }
+    }
+    /**
+     * buildings: 二维数组，输入的大楼们
+     */
+    public static List<List<Interger>> BuildingOutline(int[][] buildings) {
+        // 新建一个长度为 2 倍大楼数量的 node 数组，用来记录每一栋大楼的上和下位置的信息
+        Node[] nodes = new Node[buildings.length * 2];
+        for (int i = 0; i < buildings.length; i++) {
+            // true 记录上的信息
+            nodes[i * 2] = new Node(true, buildings[i][0], buildings[i][2])
+            // false 记录下的信息
+            nodes[i * 2 + 1] = new Node(false, buildings[i][1], buildings[i][2])
+        }
+        // 根据位置排序
+        Arrays.sort(nodes, new NodeComparator())
+        // 二叉平衡搜索树
+        // htMap 用来记录遍历过程
+        TreeMap<Interger, Interger> htMap = new TreeMap<>()
+        // pmMap用来记录每一个位置的最大高度
+        TreeMap<Interger, Interger> pmMap = new TreeMap<>()
+        // 遍历排序后的 nodes 数组
+        for (int i = 0; i < nodes.length; i++){
+            // 如果是往上就 加
+            if(nodes[i].isUp) {
+                // 以 h 为 key, 出现的次数为 value
+                if (!htMap.containsKey(nodes[i].h)) {
+                    htMap.put(nodes[i].h, 1)
+                } else {
+                    // 出现过得话在原有的基础上加 1
+                    htMap.put(nodes[i].h, htMap.get(nodes[i].h) + 1)
+                }
+            } else {
+                // 如果是下就 减
+                if (htMap.containsKey(nodes[i].h)) {
+                    if (htMap.get(nodes[i].h) == 1) {
+                        htMap.remove(nodes[i].h)
+                    } else {
+                        htMap.put(nodes[i].h, htMap.get(nodes[i].h) - 1)
+                    }
+                }
+            }
+            // 每遍历一个位置都要把当前位置的最大高度存储起来
+            if (htMap.isEmpty()) {
+                pmMap.put(nodes[i].posi, 0)
+            } else {
+                // htMap.lastKey 为获取当前树种的最大高度
+                pmMap.put(nodes[i].posi, htMap.lastKey())
+            }
+        }
+        // 根据遍历得到的结果 pmMap， 生成轮廓
+        List<List<Interger>> res = new ArrayList<>()
+        int start = 0;
+        int height = 0
+        for(Entry<Integer, Integer> entry : pmMap.entrySet()) {
+            int curPosition = entry.gertKey()
+            int curMaxHeight = entry.getValue()
+            // 如果高度发生了变化
+            if (height != curMaxHeight) {
+                if (height != 0) {
+                    List<Interger> newRecord = new ArrayList<Interger>()
+                    newRecord.add(start)
+                    newRecord.add(curPosition)
+                    newRecord.add(height)
+                    res.add(newRecord)
+                }
+                start = curPosition
+                height = curMaxHeight
+            }
+        }
+        return res
+    }
+
+
+}
